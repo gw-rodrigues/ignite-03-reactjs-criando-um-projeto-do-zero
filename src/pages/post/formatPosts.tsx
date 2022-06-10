672307205c7efd7a111d7d1744dd9880562f81a9
Gleydson Rodrigues <rodrigues.gw@gmail.com>
2022-06-10 10:57:14 +0200

    improving data

--- src/pages/post/formatPosts.tsx
+++ src/pages/post/formatPosts.tsx
@@ -1,7 +1,26 @@
 import { PrismicDocument } from '@prismicio/types';
 import { format } from 'date-fns';
 import ptBR from 'date-fns/locale/pt-BR';
-
+/*
+    alternate_languages: []
+    data:
+        author: "Joseph Oliveira"
+        banner: {dimensions: {…}, alt: null, copyright: null, url: 'https://images.prismic.io/spacetraveling-gwrodrigu…iando-um-app-cra-do-zero.jpg?auto=compress,format'}
+        content: (2) [{…}, {…}]
+        subtitle: "Pensando em sincronização em vez de ciclos de vida."
+        title: "Como utilizar Hooks"
+    first_publication_date: "2022-06-03T21:12:51+0000"
+    href: "https://spacetraveling-gwrodrigues.cdn.prismic.io/api/v2/documents/search?ref=YqJt0hEAACIAO8Db&q=%5B%5B%3Ad+%3D+at%28document.id%2C+%22Ypp5GhEAACIAGF8v%22%29+%5D%5D"
+    id: "Ypp5GhEAACIAGF8v"
+    lang: "pt-br"
+    last_publication_date: "2022-06-09T22:01:54+0000"
+    linked_documents: []
+    slugs: (2) ['como-utilizar-hooks', 'criando-um-app-cra-do-zero']
+    tags: []
+    type: "posts"
+    uid: "como-utilizar-hooks-2"
+    url: null
+*/ /*
 interface Post {
   uid?: string;
   first_publication_date: string | null;
@@ -11,6 +30,25 @@ interface Post {
     author: string;
     banner?: string;
     content?: string;
+  };
+} */
+
+interface Post {
+  uid: string;
+  first_publication_date: string | null;
+  data: {
+    title: string;
+    subtitle: string;
+    banner: {
+      url: string;
+    };
+    author: string;
+    content: {
+      heading: string;
+      body: {
+        text: string;
+      }[];
+    }[];
   };
 }
 
